c3af4a73a2685e1ca4e5b51cbe08fe51e4613478
Allison Zhao <allisonzhao.uni@gmail.com>
2024-08-17 17:52:39 -0400

    add logo and slogan on introduction screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View} from 'react-native';
+import { Image, Text, View} from 'react-native';
 import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
+import { images } from '../constants';
 
 export default function App() {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View className="w-full flex justify-center items-center h-full px-4">
+          <Image source={images.logo} style={{ width:400, height:400 }}/>
+          <View className="relative mt-5">
+            <Text className="text-3xl text-black font-bold text-center">
+              Slogan here
+            </Text>
+            {/* <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+            /> */}
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
