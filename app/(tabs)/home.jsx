8d544e1da7ce8636d1dba1ce512d9ce753cd4218
Allison Zhao <allisonzhao.uni@gmail.com>
2024-08-17 21:51:34 -0400

    basic layout for home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import { images } from '../../constants'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+        <FlatList 
+            data={[{ id: 1 }, { id: 2 }, { id: 3 }]} 
+            keyExtractor={item => item.id.toString()}
+            renderItem={({ item }) => <Text className="text-3xl">{item.id}</Text>}
+            ListHeaderComponent={() => (
+                <View className="my-6 px-4 space-y-6">
+                    <View className="justify-between items-start flex-row mb-6">
+                        <View>
+                            <Text className="text-2xl font-psemibold">
+                                Welcome!
+                            </Text>
+                        </View>
+
+                        <View className="mt-0">
+                            <Image 
+                                source={images.logo}
+                                className="w-20 h-20"
+                            />
+                        </View>
+                    </View>
+
+                </View>
+            )}
+            />
+
+    </SafeAreaView>
   )
 }
 
